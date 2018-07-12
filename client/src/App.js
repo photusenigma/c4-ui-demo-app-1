809ae2a94f633159a003cbf1b35a70d90c113a98
WheatleyTheCore <alex.colwell@icloud.com>
2018-07-12 17:22:43 -0400

    chore: created react app that connects to db with apollo

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "simple api url"
+});
+
+client
+.query({
+  query: gql`
+    {
+      rates(currency: "USD") {
+        currency
+      }
+    }
+  `
+})
+.then(result => console.log(result));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
